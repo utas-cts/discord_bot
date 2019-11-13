ecbe321d58c5ccc0392c6d8c8b117ad28aef94e1
danielcornwell-coder <56154691+danielcornwell-coder@users.noreply.github.com>
2019-11-13 22:14:22 +1100

    Create main.py
    Discord bot to welcome new members

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord')
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, Welcome to the CTS server, say hi to everyone :)'
+    )
+    print(f'Sent a message to {member.name}')
+
+F = open("Credentials.txt","r")
+DISCORD_TOKEN = str(F.read().rstrip('\n'))
+F.close()
+
+client.run(str(DISCORD_TOKEN))
